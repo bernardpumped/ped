76fb449b8f8e2e9e200527f0ab77ecd428a78704
bernardcraddock <bernard@pumpedfuel.com>
2022-09-06 20:40:17 +1000

    remove unused imports

--- lib/user-interface/fuel-station-details/screen/tabs/contact/widget/rate_widget.dart
+++ lib/user-interface/fuel-station-details/screen/tabs/contact/widget/rate_widget.dart
@@ -24,7 +24,6 @@ import 'package:pumped_end_device/models/pumped/fuel_station_address.dart';
 import 'package:pumped_end_device/user-interface/fuel-station-details/screen/tabs/contact/widget/feature_support.dart';
 import 'package:pumped_end_device/user-interface/utils/widget_utils.dart';
 import 'package:pumped_end_device/util/log_util.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class RateWidget extends StatelessWidget {
